3f5c1b86adadfc13e5d49a68e9d0462e7a3a1619
Avinash <Avinash@zopnote.com>
2025-12-04 08:12:29 +0530

    add open panel

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { OpenPanelComponent } from "@openpanel/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
