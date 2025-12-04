8084146eba688ef114405e1257194734b157df03
Avinash <Avinash@zopnote.com>
2025-12-04 08:01:35 +0530

    add open panel

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,6 +20,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <OpenPanelComponent
+          clientId={process.env.OPEN_PANEL_CLIENT_ID}
+          trackScreenViews={true}
+        // trackAttributes={true}
+        // trackOutgoingLinks={true}
+        // If you have a user id, you can pass it here to identify the user
+        // profileId={'123'}
+        />
         <script
           src="https://rybbit.shadrepo.xyz/api/script.js"
           data-site-id="1"
